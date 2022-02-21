3f9f6d27cedc9cefd1bacff3e7a1c9e7258c76a1
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-21 13:33:21 -0500

    git init: Clone starter project

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='mx-auto mt-48 text-center'>
+      <p>Art from Joanne Li @joanne on Figma <a href='https://keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
+    </footer>
+  )
+}
