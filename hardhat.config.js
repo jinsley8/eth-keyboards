3432155bb71f2138e48182ea31a8e375542e8809
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-21 15:21:15 -0500

    feat: Add network config to hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +8,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.QUICKNODE_API_ENDPOINT,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
 };
