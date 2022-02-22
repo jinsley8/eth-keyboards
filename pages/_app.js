2fd4d23eb717f0d5fc26d861ecb90b52be1f0553
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-21 20:02:49 -0500

    feat: Add toast notification component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
