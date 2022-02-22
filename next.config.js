dc1fe195971f1b0c25103658f54dca5de4d90003
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-21 21:36:00 -0500

    feat: Add environment variables to the JavaScript bundle

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    WALLET_PRIVATE_KEY: process.env.WALLET_PRIVATE_KEY,
+    QUICKNODE_API_ENDPOINT: process.env.QUICKNODE_API_ENDPOINT
+  },
 }
